34c491c15c59a317510e5a564ef98d507fd9966c
Steven McGrath <mcgrath.steven7@gmail.com>
2019-07-03 11:57:24 -0400

    chore: removed firebase config from repo

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -3,15 +3,7 @@ const admin = require('firebase-admin');
 const express = require('express');
 const firebase = require('firebase');
 
-const firebaseConfig = {
-  apiKey: 'AIzaSyAx-q9UOGbHAmex5_u1gkgvnlLD4E8YSCA',
-  authDomain: 'postr-7e7e0.firebaseapp.com',
-  databaseURL: 'https://postr-7e7e0.firebaseio.com',
-  projectId: 'postr-7e7e0',
-  storageBucket: 'postr-7e7e0.appspot.com',
-  messagingSenderId: '353182075833',
-  appId: '1:353182075833:web:9a2af7e6dd6621f5',
-};
+const firebaseConfig = require('./firebaseconfig.json');
 
 const app = express();
 
